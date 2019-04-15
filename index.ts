795f13272c3c64c0c7528345690060189497a0bd
Zhe Li <zlhe@microsoft.com>
2019-04-15 14:11:54 +0800

    add import job class

--- index.ts
+++ index.ts
@@ -2,98 +2,95 @@
 
 import {join} from 'path';
 
-const importQueue:string[] = [];
-let importJobBusy = false;
-
 function impor<T extends object>(id: string, rootDir: string): T {
   if (/^\./.test(id)) {
     id = join(rootDir, id);
   }
-  if (importQueue.indexOf(id) === -1) {
-    importQueue.push(id);
+  if (ImportJob.importQueue.indexOf(id) === -1) {
+    ImportJob.importQueue.push(id);
   }
   let mod: T;
   return new Proxy((() => {}) as T, {
     apply: (_, thisArg, argumentsList) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.apply(mod as Function, thisArg, argumentsList);
     },
     construct: (_, argumentsList) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.construct(mod as Function, argumentsList);
     },
     defineProperty: (_, key, attributes) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.defineProperty(mod, key, attributes);
     },
     deleteProperty: (_, key) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.deleteProperty(mod, key);
     },
     get: (_, key, reciver) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.get(mod, key, reciver);
     },
     getPrototypeOf: (_) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.getPrototypeOf(mod);
     },
     has: (_, key) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.has(mod, key);
     },
     isExtensible: (_) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.isExtensible(mod);
     },
     ownKeys: (_) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return mod ? Reflect.ownKeys(mod) : [];
     },
     preventExtensions: (_) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.preventExtensions(mod);
     },
     set: (_, key, value, reciver) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.set(mod, key, value, reciver);
     },
     setPrototypeOf: (_, value) => {
       if (!mod) {
-        removeJob(id);
+        ImportJob.removeJob(id);
         mod = require(id);
       }
       return Reflect.setPrototypeOf(mod, value);
@@ -101,33 +98,44 @@ function impor<T extends object>(id: string, rootDir: string): T {
   });
 }
 
-function removeJob(id: string) {
-  const index = importQueue.indexOf(id);
-  if (index === -1) {
-    return;
+class ImportJob {
+  private static isStarted = false;
+  static importQueue:string[] = [];
+  static importJobBusy = false;
+  static async importJob() {
+    if (ImportJob.importJobBusy) {
+      return;
+    }
+    if (ImportJob.importQueue.length === 0) {
+      return;
+    }
+  
+    const id = ImportJob.importQueue.shift();
+    if (!id) {
+      return;
+    }
+    ImportJob.importJobBusy = true;
+    await import(id);
+    ImportJob.importJobBusy = false;
   }
-
-  importQueue.splice(index, 1);
+  static removeJob(id: string) {
+    const index = ImportJob.importQueue.indexOf(id);
+    if (index === -1) {
+      return;
+    }
+  
+    ImportJob.importQueue.splice(index, 1);
+  }
+  static start() {
+    if (ImportJob.isStarted) {
+      return;
+    }
+    ImportJob.isStarted = true;
+    setInterval(ImportJob.importJob, 1000);
+  }
 }
 
-async function importJob() {
-  if (importJobBusy) {
-    return;
-  }
-  if (importQueue.length === 0) {
-    return;
-  }
-
-  const id = importQueue.shift();
-  if (!id) {
-    return;
-  }
-  importJobBusy = true;
-  await import(id);
-  importJobBusy = false;
-}
-
-setInterval(importJob, 1000);
+ImportJob.start();
 
 export = (rootDir: string) => {
   return (id: string) => {
